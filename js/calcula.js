b7a969eb851717a601e4dee45b0a3c3438c6e64f
Johnny Marques <johnnymsgouveia@gmail.com>
2024-05-24 16:15:15 -0300

    Alterado o cod js para que fosse possível efetuar a conta com mais de 3 números

--- js/calcula.js
+++ js/calcula.js
@@ -2,135 +2,98 @@ var teclado = document.querySelector(".teclado");
 var teclas = document.querySelectorAll(".tecla");
 var campo = document.querySelector(".campo")
 
-var numero1 = "";
-var numero2 = "";
-var numero3 = "";
-var operador1 = "";
-var operador2 = "";
+var numeros = [];
+var operadores = [];
 var resultado = 0;
+var ultimoTexto = "";
+
 
 teclas.forEach(function (tecla) {
     tecla.addEventListener("click", function () {
         var textoTecla = tecla.textContent;
 
-        if (tecla.textContent == "C") {
+        if (textoTecla == "C") {
             campo.value = "0";
-            numero1 = "";
-            numero2 = "";
-            numero3 = "";
-            operador1 = "";
-            operador2 = "";
+            numeros = [];
+            operadores = [];
             resultado = 0;
+            ultimoTexto = "";
             console.clear();
         } else if (textoTecla == "+" || textoTecla == "-" || textoTecla == "X" || textoTecla == "/") {
-            if (numero2 !== "") {
-                operador2 = textoTecla;
-                campo.value = campo.value + " " + operador2 + " ";
-            } else if (numero1 !== "") {
-                operador1 = textoTecla;
-                campo.value = campo.value + " " + operador1 + " ";
-            } 
+            if (ultimoTexto !== "+" && ultimoTexto !== "-" && ultimoTexto !== "X" && ultimoTexto !== "/") {
+                operadores.push(textoTecla);
+                campo.value = campo.value + " " + textoTecla + " ";
+                ultimoTexto = textoTecla;
+            }
+
         } else if (textoTecla == "=") {
-            if (numero3 !== "" && operador2 !== "") {
-                var floatNumero1 = parseFloat(numero1);
-                var floatNumero2 = parseFloat(numero2);
-                var floatNumero3 = parseFloat(numero3);
-
-                if (operador2 == "X" || operador2 == "/") {
-                    
-                    resultado = operador2 == "X" 
-                        ? floatNumero2 * floatNumero3 
-                        : floatNumero2 / floatNumero3;
-
-                    switch (operador1) {
-                        case "+":
-                            resultado += floatNumero1;
-                            break;
-                        case "-":
-                            resultado -= floatNumero1;
-                            break;
-                        case "X":
-                            resultado *= floatNumero1;
-                            break;
-                        case "/":
-                            resultado /= floatNumero1;
-                            break;
-                    }
-                } else if (operador1 == "X" || operador1 == "/") {
-
-                    resultado = operador1 == "X" 
-                        ? floatNumero1 * floatNumero2 
-                        : floatNumero1 / floatNumero2;
-
-                    switch (operador2) {
-                        case "+":
-                            resultado += floatNumero3;
-                            break;
-                        case "-":
-                            resultado -= floatNumero3;
-                            break;
-                        case "X":
-                            resultado *= floatNumero3;
-                            break;
-                        case "/":
-                            resultado /= floatNumero3;
-                            break;
-                    }
-                } else {
-                    resultado = operador1 == "+" 
-                        ? floatNumero1 + floatNumero2 
-                        : floatNumero1 - floatNumero2;
-
-                    switch (operador2) {
-                        case "+":
-                            resultado += floatNumero3;
-                            break;
-                        case "-":
-                            resultado -= floatNumero3;
-                            break;
-                }
-            }
-
-                campo.value = resultado;
-                console.log(resultado);
-            } else if ((numero1 !== "" && numero2 !== "") && operador1 !== "") {
-                var floatNumero1 = parseFloat(numero1);
-                var floatNumero2 = parseFloat(numero2);
-                switch (operador1) {
-                    case "+":
-                        resultado = floatNumero1 + floatNumero2;
-                        break;
-                    case "-":
-                        resultado = floatNumero1 - floatNumero2;
-                        break;
-                    case "X":
-                        resultado = floatNumero1 * floatNumero2;
-                        break;
-                    case "/":
-                        resultado = floatNumero1 / floatNumero2;
-                        break;
-                }
-                campo.value = resultado;
-                console.log(resultado);
-            }
+            calcularResultado();
+            ultimoTexto = textoTecla;
         } else {
             if (campo.value == "0") {
                 campo.value = "";
             }
+            numeros.push(textoTecla);
             campo.value = campo.value + textoTecla;
-            if (operador1 == "") {
-                numero1 = numero1 + textoTecla;
-                console.log("Esse é o número 1: " + numero1);
-            } else if (operador1 !== "" && operador2 == "") {
-                numero2 = numero2 + textoTecla;
-                console.log("Esse é o número 2: " + numero2);
-            } else if (operador2 !== "") {
-                numero3 = numero3 + textoTecla;
-                console.log("Esse é o número 3: " + numero3);
-            } else {
-                console.log("Não tratado!");
-            }
+            ultimoTexto = textoTecla;
         }
     });
 });
 
+function calcularResultado() {
+    var separador = campo.value.split(' ');
+    var tempNumero = "";
+    var numerosOperacao = [];
+    var operadoresOperacao = [];
+
+    separador.forEach(function (texto) {
+        if (texto !== "+" && texto !== "-" && texto !== "X" && texto !== "/") {
+            texto == "" ? texto = 0 : null;
+            tempNumero += texto;
+            numerosOperacao.push(parseFloat(tempNumero));
+            tempNumero = '';
+        } else {
+            operadoresOperacao.push(texto);
+        }
+    });
+
+    console.log("=================");
+    console.log(numerosOperacao);
+    console.log(operadoresOperacao);
+
+    for (var i = 0; i < numerosOperacao.length; i++) {
+        if (operadoresOperacao[i] == "X" || operadoresOperacao[i] == "/") {
+            if (operadoresOperacao[i] == "X") {
+                numerosOperacao[i + 1] = numerosOperacao[i] * numerosOperacao[i + 1];
+            } else {
+                numerosOperacao[i + 1] = numerosOperacao[i] / numerosOperacao[i + 1];
+            }
+            numerosOperacao[i] = null;
+            operadoresOperacao[i] = null;
+        }
+    }
+    
+    numerosOperacao = numerosOperacao.filter(num => num !== null);
+    operadoresOperacao = operadoresOperacao.filter(op => op !== null);
+
+    console.log("=================");
+    console.log(numerosOperacao);
+    console.log(operadoresOperacao);
+    console.log("=================");
+
+    for (var i = 0; i < numerosOperacao.length; i++) {
+        if (operadoresOperacao[i] == "+" || operadoresOperacao[i] == "-") {
+            if (operadoresOperacao[i] == "+") {
+                numerosOperacao[i + 1] = numerosOperacao[i] + numerosOperacao[i + 1];
+            } else {
+                numerosOperacao[i + 1] = numerosOperacao[i] - numerosOperacao[i + 1];
+            }
+        }
+    }
+
+    resultado = numerosOperacao[numerosOperacao.length - 1];
+    console.log("Resultado: " + resultado);
+
+    campo.value = resultado;
+
+}
